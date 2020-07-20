4feb45d530a34f56f194911e0daefbd3920060a4
Isaac Cantún <izakntun@gmail.com>
2020-07-20 09:48:11 -0500

    Curso de creación de APIs con Rails y TDD con RSpec

---
+++ test/factories/posts.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :post do
+    title { Faker::Lorem.sentence }
+    content { Faker::Lorem.paragraph }
+    published { 
+      r = rand(0..1)
+
+      true ? r == 1 : false
+     }
+    user
+  end
+
+  factory :published_post, class: 'Post' do
+    title { Faker::Lorem.sentence }
+    content { Faker::Lorem.paragraph }
+    published { true }
+    user
+  end
+end
